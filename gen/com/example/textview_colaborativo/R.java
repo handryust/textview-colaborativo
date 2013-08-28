78c317c07eacc25d30c9bd9dcf6364458dec21c9
flukke-note <pm5k.sk@gmail.com>
2013-08-28 00:45:44 -0400

    new button for robinson

--- gen/com/example/textview_colaborativo/R.java
+++ gen/com/example/textview_colaborativo/R.java
@@ -24,13 +24,14 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080007;
+        public static final int action_settings=0x7f080008;
         public static final int btn_ale=0x7f080006;
-        public static final int btn_evelyn=0x7f080001;
+        public static final int btn_evelyn=0x7f080002;
         public static final int btn_mario=0x7f080005;
         public static final int btn_pam=0x7f080004;
+        public static final int btn_robinson=0x7f080007;
         public static final int btn_samuel=0x7f080003;
-        public static final int btn_victor=0x7f080002;
+        public static final int btn_victor=0x7f080001;
         public static final int textView1=0x7f080000;
     }
     public static final class layout {
