de8620c12103348953dfd16d94b61c2bbbac83d1
Mario-Alonso <gutierrez.briones@gmail.com>
2013-08-29 20:42:29 -0400

    agregue una clase con el mensaje hola mundo

---
+++ src/com/example/textview_colaborativo/MGutierrez.java
@@ -0,0 +1,10 @@
+package com.example.textview_colaborativo;
+
+/**
+ * Created by ammen on 29-08-13.
+ */
+public class MGutierrez {
+    public String getTex(){
+        return "ejemplo uno";
+    }
+}
